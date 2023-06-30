a2662097b6c9f09f0c294905f8672fda8f475d8f
Ganesh-2134 <120586188+Ganesh-2134@users.noreply.github.com>
2023-06-30 14:39:16 +0530

    Add files via upload
    uploaded tf modules for ecs cluster

---
+++ terraform.tfvars
@@ -0,0 +1,13 @@
+aws_region        = "us-east-1"
+aws_access_key    = "your aws access key"
+aws_secret_key    = "your aws secret key"
+
+availability_zones = ["us-east-1a", "us-east-1b"]
+
+private_subnets    = "" #private subnet id
+
+vpc_id = " " # vpc id 
+
+
+app_name        = "node-js-app"
+app_environment = "production"
---
+++ variable.tf
@@ -0,0 +1,44 @@
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Key"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS Region"
+}
+
+variable "aws_cloudwatch_retention_in_days" {
+  type        = number
+  description = "AWS CloudWatch Logs Retention in Days"
+  default     = 1
+}
+
+variable "app_name" {
+  type        = string
+  description = "Application Name"
+}
+
+variable "app_environment" {
+  type        = string
+  description = "Application Environment"
+}
+
+variable "private_subnets" {
+  description = "List of private subnets"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+}
+
+variable "vpc_id" {
+  description = "Enter the Vpc id"
+}
+
+
